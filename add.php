cc212d751de64f629ebfc292d47c4f6dc4a2b7f1
Mark Howard <markhoward2005@gmail.com>
2024-05-24 15:01:43 +0000

    Changed UID Fetch

--- add.php
+++ add.php
@@ -107,7 +107,10 @@
         $name = $_POST["name"];
         $streak = $_POST["streak"];
         $evidence = $_POST["evidence"];
-        $new_id = $row['uid'] + 1;
+
+        $result = $conn->query("SELECT MAX(uid) AS max_id FROM scores");
+        $row = $result->fetch_assoc();
+        $new_id = $row['max_ui'] + 1;
         
         $servername = "localhost";
         $username = "root";
